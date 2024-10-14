fc4f22b38eb3f743c4b6e46e2d84af09b5329c12
Guilherme Fortuna <guilherme.fferreira@goias.gov.br>
2024-10-14 16:14:02 -0300

    refactor: Migrando para o Bootstrap

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,36 +1,39 @@
-<div name="Footer section" id="footer" class="border-t border-paragraph-400">
-    <div class="py-6 grid grid-cols-2">
-        <div class="flex flex-col">
-            <div class="flex">
+<div name="Footer section" id="footer" class="border-top border-secondary">
+    <div class="py-4 row">
+        <div class="col-6 d-flex flex-column">
+            <div class="d-flex">
                 <img src="{{ asset('/assets/logo.png') }}" class="logo-image">
                 <p class="logo-text">Lalasia</p>
             </div>
-            <div class="flex pt-8">
-                <p3 class="text-black">Lalasia is digital agency that help you make better experience iaculis cras in.</p3>
+            <div class="pt-4">
+                <p class="text-dark">Lalasia is a digital agency that helps you make better experiences iaculis cras in.</p>
             </div>
         </div>
 
-        <div class="grid grid-cols-3 place-self-end gap-x-12">
-            <div class="flex flex-col gap-y-4">
-                <h4>Product</h4>
-                <a href="#"><p3>New Arrivals</p3></a>
-                <a href="#"><p3>Best Selling</p3></a>
-                <a href="#"><p3>Home Decor</p3></a>
-                <a href="#"><p3>Kitchen Set</p3></a>
-            </div>
+        <div class="col-6 d-flex justify-content-end">
+            <div class="row">
+                <div class="col d-flex flex-column gap-2">
+                    <h4>Product</h4>
+                    <a href="#"><p>New Arrivals</p></a>
+                    <a href="#"><p>Best Selling</p></a>
+                    <a href="#"><p>Home Decor</p></a>
+                    <a href="#"><p>Kitchen Set</p></a>
+                </div>
 
-            <div class="flex flex-col gap-y-4">
-                <h4>Services</h4>
-                <a href="#"><p3>Catalog</p3></a>
-                <a href="#"><p3>Blog</p3></a>
-                <a href="#"><p3>FaQ</p3></a>
-                <a href="#"><p3>Pricing</p3></a>
-            </div>
-            <div class="flex flex-col gap-y-4">
-                <h4>Follow Us</h4>
-                <a href="#"><p3>Facebook</p3></a>
-                <a href="#"><p3>Instagram</p3></a>
-                <a href="#"><p3>Twitter</p3></a>
+                <div class="col d-flex flex-column gap-2">
+                    <h4>Services</h4>
+                    <a href="#"><p>Catalog</p></a>
+                    <a href="#"><p>Blog</p></a>
+                    <a href="#"><p>FaQ</p></a>
+                    <a href="#"><p>Pricing</p></a>
+                </div>
+
+                <div class="col d-flex flex-column gap-2">
+                    <h4>Follow Us</h4>
+                    <a href="#"><p>Facebook</p></a>
+                    <a href="#"><p>Instagram</p></a>
+                    <a href="#"><p>Twitter</p></a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,7 @@
         'resources/sass/app.scss',
         'resources/js/app.js',
         'resources/css/app.css',
+        'resources/css/iconsax.css',
         ])
         <title>4Selet - Furniture Website (Avaliação)</title>
     </head>
@@ -29,150 +30,117 @@
         <x-header />
 
         <!-- Welcome section -->
-        <div name="Welcome section"
-            class="border-bottom">
-            <div class>
+        <div name="Welcome section" class="border-bottom">
+            <div class="text-center">
                 <h1>Discover Furniture With</h1>
                 <h1>High Quality Wood</h1>
             </div>
-            <div class>
-                <p2 class>Pellentesque etiam blandit in tincidunt at donec.
-                    Eget ipsum dignissim placerat nisi, adipiscing mauris
-                    non. Purus parturient viverra nunc, tortor sit laoreet.
-                    Quam tincidunt aliquam adipiscing tempor.</p3>
+            <div class="text-center">
+                <p class="text-muted">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi,
+                    adipiscing mauris non. Purus parturient viverra nunc, tortor sit laoreet. Quam tincidunt aliquam
+                    adipiscing tempor.</p>
             </div>
             <x-searchbar />
-            <div name="image" class>
-                <img class="mx-auto" src="{{ asset('/assets/img1.png') }}"
-                    alt="Lalasia Logo">
-
+            <div name="image" class="text-center">
+                <img class="mx-auto" src="{{ asset('/assets/img1.png') }}" alt="Lalasia Logo">
             </div>
         </div>
 
         <!-- Benefits section -->
-        <div name="Benefits section" class>
-            <div
-                class="grid grid-cols-2 lg:flex-row place-content-around gap-x-60">
-                <div class="flex flex-col">
+        <div name="Benefits section" class="container py-5">
+            <div class="row justify-content-between">
+                <div class="col-md-6">
                     <h5 class="text-secondary">Benefits</h5>
-                    <h2>Benifits when using our services</h2>
+                    <h2>Benefits when using our services</h2>
                 </div>
-                <div class="self-center">
-                    <p3>Pellentesque etiam blandit in tincidunt at donec.
-                        Eget ipsum dignissim placerat nisi, adipiscing
-                        mauris non purus parturient.</p3>
+                <div class="col-md-6 align-self-center">
+                    <p class="text-muted">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat
+                        nisi, adipiscing mauris non purus parturient.</p>
                 </div>
             </div>
-            <div class="grid grid-cols-3 gap-4 col-span-3">
-                <div class="flex flex-col col-start-1">
-                    <img class="size-16"
-                        src="{{ asset('/assets/3square.png') }}"
-                        alt="Many Choices">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid" src="{{ asset('/assets/3square.png') }}" alt="Many Choices">
                     <h3>Many Choices</h3>
-                    <p3 class="pt-2">Pellentesque etiam blandit in tincidunt at
-                        donec. Eget ipsum dignissim placerat
-                        nisi, adipiscing mauris non.</p3>
+                    <p class="pt-2">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi,
+                        adipiscing mauris non.</p>
                 </div>
 
-                <div class="col-start-2 my-auto">
-                    <img class="size-10"
-                        src="{{ asset('/assets/calendar-tick.png') }}"
-                        alt="Many Choices">
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid" src="{{ asset('/assets/calendar-tick.png') }}" alt="Fast and On Time">
                     <h3 class="my-2">Fast and On Time</h3>
-                    <p3 class>Pellentesque etiam blandit in tincidunt at donec.
-                        Eget ipsum dignissim placerat nisi,
-                        adipiscing mauris non.</p3>
+                    <p>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing
+                        mauris non.</p>
                 </div>
 
-                <div class="col-start-3 "><img class="size-10"
-                        src="{{ asset('/assets/money-tick.png') }}"
-                        alt="Many Choices">
+                <div class="col-md-4 text-center">
+                    <img class="img-fluid" src="{{ asset('/assets/money-tick.png') }}" alt="Affordable Price">
                     <h3>Affordable Price</h3>
-                    <p3 class="pt-4">Pellentesque etiam blandit in tincidunt at
-                        donec. Eget ipsum dignissim placerat
-                        nisi, adipiscing mauris non.</p3>
+                    <p class="pt-4">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi,
+                        adipiscing mauris non.</p>
                 </div>
             </div>
         </div>
 
         <!-- Our product section -->
-        <div name="Our product section" class="py-12">
-            <div class="flex flex-col mx-auto text-center">
-                <h5 class="text-secondary">Product</h5>
-                <h2>Our popular product</h2>
-                <p3 class="pt-4">Pellentesque etiam blandit in tincidunt at
-                    donec. Eget ipsum dignissim placerat
-                    nisi, adipiscing mauris non.</p3>
-            </div>
-            <h3 class="mx-auto">Implementar carrossel</h3>
+        <div name="Our product section" class="py-5 text-center">
+            <h5 class="text-secondary">Product</h5>
+            <h2>Our popular product</h2>
+            <p class="pt-4">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing
+                mauris non.</p>
+            <h3>Implementar carrossel</h3>
         </div>
 
         <!-- Our product section -->
-        <div name="Our product section" class="grid grid-cols-2">
-            <div class>
+        <div name="Our product section" class="row">
+            <div class="col-md-6">
                 <h5 class="text-secondary">Our product</h5>
-                <h2>Our Product Crafted by talented and high quality
-                    material</h2>
-                <div class="self-center">
-                    <p3>Pellentesque etiam blandit in tincidunt at donec. Eget
-                        ipsum dignissim placerat nisi, adipiscing
-                        mauris non purus parturient. morbi fermentum, vivamus et
-                        accumsan dui tincidunt pulvinar</p3>
-                </div>
-                <button name="Learn more"
-                    class="bg-primary text-white mx-auto px-12 py-4 font-semibold">Learn
-                    more</button>
-                <img src="{{ asset('/assets/img2.png') }}" alt="Imagem 2">
+                <h2>Our Product Crafted by talented and high quality material</h2>
+                <p>Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing mauris
+                    non purus parturient. morbi fermentum, vivamus et accumsan dui tincidunt pulvinar.</p>
+                <button name="Learn more" class="btn btn-primary px-5 py-3">Learn more</button>
+                <img src="{{ asset('/assets/img2.png') }}" alt="Imagem 2" class="img-fluid mt-3">
             </div>
 
-            <div class>
-                <div class="grid grid-cols-3 place-items-end">
-                    <div name="Years experience" class="mx-auto">
+            <div class="col-md-6 text-center">
+                <div class="row">
+                    <div class="col">
                         <h2>20+</h2>
-                        <p3>Years Experience</p3>
+                        <p>Years Experience</p>
                     </div>
-                    <div name="Happy clients" class="mx-auto">
+                    <div class="col">
                         <h2>483</h2>
-                        <p3>Happy Client</p3>
+                        <p>Happy Client</p>
                     </div>
-                    <div name="Project finished" class="mx-auto">
+                    <div class="col">
                         <h2>150+</h2>
-                        <p3>Project Finished</p3>
+                        <p>Project Finished</p>
                     </div>
                 </div>
-                <img src="{{ asset('/assets/img3.png') }}" alt="Imagem 3" class>
+                <img src="{{ asset('/assets/img3.png') }}" alt="Imagem 3" class="img-fluid mt-3">
             </div>
         </div>
 
-        <!-- Testimonials section  -->
-        <div name="Testimonials section" class="flex flex-col py-12">
-            <div class="flex flex-col mx-auto text-center">
-                <h5 class="text-secondary">Testimonials</h5>
-                <h2>What our customer say</h2>
-                <p3 class="pt-4">Pellentesque etiam blandit in tincidunt at
-                    donec. Eget ipsum dignissim placerat nisi,
-                    adipiscing mauris non.</p3>
-            </div>
-            <h3 class="mx-auto">Adicionar carrossel de Avaliações</h3>
+        <!-- Testimonials section -->
+        <div name="Testimonials section" class="py-5 text-center">
+            <h5 class="text-secondary">Testimonials</h5>
+            <h2>What our customer say</h2>
+            <p class="pt-4">Pellentesque etiam blandit in tincidunt at donec. Eget ipsum dignissim placerat nisi, adipiscing
+                mauris non.</p>
+            <h3>Adicionar carrossel de Avaliações</h3>
         </div>
 
         <!-- Articles section -->
-        <div name="Articles section">
-
-        </div>
-        <div name="Our product section" class="">
-            <div class="">
-                <h5 class="text-secondary">Articles</h5>
-                <h2>The best furniture comes from Lalasia</h2>
-                <div class="">
-                    <p3>Pellentesque etiam blandit in tincidunt at donec. </p3>
-                </div>
-                <h3>Card + carrossel</h3>
-            </div>
+        <div name="Articles section" class="py-5 text-center">
+            <h5 class="text-secondary">Articles</h5>
+            <h2>The best furniture comes from Lalasia</h2>
+            <p>Pellentesque etiam blandit in tincidunt at donec.</p>
+            <h3>Card + carrossel</h3>
         </div>
 
         <x-footer />
 
     </body>
+
 
 </html>
