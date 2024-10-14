e90d9b82e612d07ace16863273dee36838f09cd9
Guilherme Fortuna <guilherme.fferreira@goias.gov.br>
2024-10-14 20:26:26 -0300

    feat: header responsivo com botao offcanvas

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,49 @@
-<div name="Header section" id="header" class="border-bottom">
-    <!-- Ajustar o border para pegar a página inteira assim como no Figma -->
-    <div class="grid grid-cols-3 w-full">
-        <div class="flex gap-x-2">
+<div name="Header section" id="header" class="container-fluid">
+    <div class="row align-items-center py-3">
+        <!-- Logo and Title header -->
+        <div class="col-6 col-md-4 d-flex align-items-center">
             <img src="{{ asset('/assets/logo.png') }}" class="logo-image">
-            <p class="logo-text">Lalasia</p>
+            <p class="logo-text mb-0 ms-2">Lalasia</p>
         </div>
 
-        <div class="header-buttons-center">
-            <a href="#">Product</a>
-            <a href="#">Services</a>
-            <a href="#">Article</a>
+        <!-- Button to toggle offcanvas menu in small screens -->
+        <div class="col-6 d-md-none text-end">
+            <button class="btn btn-offcanvas text-black fs-3 icon-link-hover" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+                &#9776; <!-- Hamburger Icon -->
+            </button>
+        </div>
+
+        <!-- Buttons header - visible in large screens only -->
+        <div name="Buttons header" class="col-md-4 text-center d-none d-md-block header-buttons-center">
+            <a href="#" class="header-buttons-center me-3">Product</a>
+            <a href="#" class="header-buttons-center me-3">Services</a>
+            <a href="#" class="header-buttons-center me-3">Article</a>
             <a href="#">About Us</a>
         </div>
 
-        <div class="header-buttons-end">
-            <a href="#"><x-solar-bag-5-linear class="h-8" /></a>
-            <a href="#"><x-heroicon-o-user class="h-8" /></a>
+        <!-- Icons header - visible in large screens only -->
+        <div class="col-md-4 text-end d-none d-md-block">
+            <a href="#" class="me-3">iBAG2</a>
+            <a href="#">iUSER</a>
+        </div>
+    </div>
+
+    <!-- Offcanvas menu for small screens -->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+        <div class="offcanvas-header border-bottom">
+            <img src="{{ asset('/assets/logo.png') }}" class="logo-image">
+            <p class="logo-text mb-0 ms-2">Lalasia</p>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body header-buttons-center border-bottom">
+            <!-- Buttons header inside offcanvas -->
+            <a href="#" class=" d-block mb-3">Product</a>
+            <a href="#" class="d-block mb-3">Services</a>
+            <a href="#" class="d-block mb-3">Article</a>
+            <a href="#" class="d-block pb-3 border-bottom ">About Us</a>
+            <!-- Icons header inside offcanvas -->
+            <a href="#" class="d-block my-3">iBAG2</a>
+            <a href="#" class="d-block">iUSER</a>
         </div>
     </div>
 </div>
-
