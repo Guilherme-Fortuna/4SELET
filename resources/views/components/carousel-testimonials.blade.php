8e2bc5f5530cdaaf43e8424ace11f8bfac0bc146
Guilherme Fortuna <guilherme.fferreira@goias.gov.br>
2024-10-15 00:23:00 -0300

    feat: iniciado a criação dos cards para o carrossel da Seção Testimonials

---
+++ resources/views/components/carousel-testimonials.blade.php
@@ -0,0 +1,32 @@
+<div class="row w-100">
+    <div class="shadow-sm p-4 rounded card-body col-md-3">
+        <img class="img-card-benefits"
+            src="{{ asset('/assets/3square.png') }}"
+            alt="Many Choices">
+        <h3 class="card-title my-2">Many Choices</h3>
+        <p3 class="card-text">Pellentesque etiam blandit in
+            tincidunt at donec. Eget ipsum dignissim placerat nisi,
+            adipiscing mauris non.</p3>
+    </div>
+
+    <div class="shadow-sm p-4 rounded card-body col-md-3">
+        <img class="img-card-benefits"
+            src="{{ asset('/assets/calendar-tick.png') }}"
+            alt="Fast and On Time">
+        <h3 class="card-title my-2">Fast and On Time</h3>
+        <p3 class="card-text">Pellentesque etiam blandit in
+            tincidunt at donec. Eget ipsum dignissim placerat nisi,
+            adipiscing
+            mauris non.</p3>
+    </div>
+
+    <div class="shadow-sm p-4 rounded card-body col-md-3">
+        <img class="img-card-benefits"
+            src="{{ asset('/assets/money-tick.png') }}"
+            alt="Affordable Price">
+        <h3 class="card-title my-2">Affordable Price</h3>
+        <p3 class="card-text">Pellentesque etiam blandit in
+            tincidunt at donec. Eget ipsum dignissim placerat nisi,
+            adipiscing mauris non.</p3>
+    </div>
+</div>
